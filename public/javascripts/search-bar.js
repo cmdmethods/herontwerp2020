021d0d046efbf48f1a512b2163d719d7b2fdb217
benl95 <beno_1995@hotmail.com>
2020-05-19 14:40:20 +0200

    Create debounce function and add to search function

--- public/javascripts/search-bar.js
+++ public/javascripts/search-bar.js
@@ -1,15 +1,30 @@
 const searchBar = document.forms['search-box'].querySelector('input')
 
-searchBar.addEventListener('keyup', function (e) {
-	const term = e.target.value.toLowerCase()
-	const cards = document.getElementById('card-list').children
-
-	Array.from(cards).forEach(function (card) {
-		const title = card.firstElementChild.textContent
-		if (title.toLowerCase().indexOf(term) != -1) {
-			card.classList.add('display-card')
-		} else {
-			card.classList.add('hide-card')
+const debounce = (fn, delay) => {
+	let timeoutID
+	return function (...args) {
+		if (timeoutID) {
+			clearTimeout(timeoutID)
 		}
-	})
-})
+		timeoutID = setTimeout(() => {
+			fn(...args)
+		}, delay)
+	}
+}
+
+searchBar.addEventListener(
+	'keyup',
+	debounce((e) => {
+		const term = e.target.value.toLowerCase()
+		const cards = document.getElementById('card-list').children
+
+		Array.from(cards).forEach(function (card) {
+			const title = card.firstElementChild.textContent
+			if (title.toLowerCase().indexOf(term) != -1) {
+				card.classList.add('display-card')
+			} else {
+				card.classList.add('hide-card')
+			}
+		})
+	}, 500)
+)
