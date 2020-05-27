9c0c239df459e05840ebf719229b7d7c24acfc86
benl95 <beno_1995@hotmail.com>
2020-05-27 13:08:04 +0200

    Create card component

---
+++ Desktop/cmd-methods/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div class="grid">
+      <Card
+        v-for="card in cards"
+        :key="card.id"
+        :title="card.title"
+        :text="card.text"
+        :image="card.image"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import cards from "@/data/cards.json";
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "Home",
+  components: { Card },
+  computed: {
+    cards() {
+      return cards;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+}
+</style>
