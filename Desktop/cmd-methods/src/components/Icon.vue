201ab7c1c4f3907de3731d305779d382d02823a0
benl95 <beno_1995@hotmail.com>
2020-05-28 10:54:39 +0200

    Create icon component

---
+++ Desktop/cmd-methods/src/components/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="icon">
+        <span></span>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 3rem;
+    height: 5rem;
+}
+
+span {
+    display: block;
+    width: 2.25rem;
+    height: 2.25rem;
+    background-color: #b4d457;
+    border-radius: 25px;
+}
+
+h1 {
+    margin: 0.3125rem 0 0 0;
+    font-family: var(--font-body-card);
+    font-size: 0.875rem;
+    font-weight: normal;
+    color: #b4d457;
+}
+</style>
--- Desktop/cmd-methods/src/views/Home.vue
+++ Desktop/cmd-methods/src/views/Home.vue
@@ -4,6 +4,7 @@
             <Card v-for="card in cards" :key="card.id" :title="card.title" :text="card.text" :image="card.image" />
             <Category v-for="category in categories" :key="category.id" :title="category.title" :text="category.text" :image="category.image" />
         </div>
+        <Icon v-for="category in categories" :key="category.id" :title="category.title" />
     </div>
 </template>
 
@@ -12,10 +13,11 @@ import cards from '@/data/cards.json'
 import categories from '@/data/categories.json'
 import Card from '@/components/Card.vue'
 import Category from '@/components/Category.vue'
+import Icon from '@/components/Icon.vue'
 
 export default {
     name: 'Home',
-    components: { Card, Category },
+    components: { Card, Category, Icon },
     computed: {
         cards() {
             return cards
