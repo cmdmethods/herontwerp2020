c1c926078fee469fb216cc37cf18e3c7018a2cdf
benl95 <beno_1995@hotmail.com>
2020-05-29 15:35:40 +0200

    Create search bar component

---
+++ Desktop/cmd-methods/src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="search-container">
+        <input type="search" placeholder="Search Cards" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search bar'
+}
+</script>
+
+<style scoped>
+input {
+    width: 28.125rem;
+    height: 1.5625rem;
+    border: none;
+    background-color: #ececed;
+    border-radius: 3px;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 0.875rem;
+    text-align: center;
+    background-repeat: no-repeat;
+}
+</style>
