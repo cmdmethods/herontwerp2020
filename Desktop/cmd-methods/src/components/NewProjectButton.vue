b1188d984fba3ff85e717cd4ef1ced819350c0e5
benl95 <beno_1995@hotmail.com>
2020-05-29 15:36:05 +0200

    Create new project button component

---
+++ Desktop/cmd-methods/src/components/NewProjectButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="new-project-button"><span>&#43;</span>New Project</button>
+</template>
+
+<script>
+export default {
+    name: 'New Project button'
+}
+</script>
+
+<style scoped>
+.new-project-button {
+    width: 7.75rem;
+    height: 2rem;
+    background-color: #5cc7ee;
+    border: none;
+    border-radius: 3px;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 600;
+    color: #ffffff;
+    font-size: 1rem;
+}
+
+span {
+    margin: 0 0.2rem 0 0;
+    font-weight: 900;
+    font-family: 'Source Sans Pro', sans-serif;
+}
+</style>
