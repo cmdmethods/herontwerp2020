ce490b7f1643d2bd3a773df0b4016929bf9d2984
rijkvanzanten <rijkvanzanten@me.com>
2020-06-02 10:26:32 -0400

    Group cards on home by category

---
+++ src/components/CategoryHeader.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="category-header" :class="id">
+        <h1 :class="`color-${id}`">
+            {{ name }} <i>{{ motto }}</i>
+        </h1>
+        <div class="line" :class="`bg-${id}`" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        id: String,
+        motto: String
+    }
+}
+</script>
+
+<style scoped>
+.category-header {
+    width: 100%;
+    position: relative;
+}
+
+h1 {
+    background-color: var(--body-background);
+    position: relative;
+    z-index: 2;
+    width: max-content;
+    padding-right: 1em;
+    display: flex;
+    align-items: center;
+}
+
+i {
+    font-weight: 400;
+    font-size: 1rem;
+    margin-left: 1em;
+}
+
+.line {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 100%;
+    transform: translateY(-50%);
+    height: 4px;
+    border-radius: 2px;
+    z-index: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="home">
         <Header />
-        <div class="grid">
-            <Card v-for="card in cards" :key="card.id" v-bind="card" />
+
+        <div class="category" v-for="category in cardsGrouped" :key="category.id">
+            <CategoryHeader v-bind="category" />
+
+            <div class="grid">
+                <Card v-for="card in category.cards" :key="card.id" v-bind="card" />
+            </div>
         </div>
     </div>
 </template>
@@ -13,31 +18,35 @@ import categories from '@/data/categories.json'
 import info from '@/data/info.json'
 import Header from '@/components/Header.vue'
 import Card from '@/components/Card.vue'
+import CategoryHeader from '@/components/CategoryHeader.vue'
 
 export default {
     name: 'Home',
-    components: { Header, Card },
+    components: { Header, Card, CategoryHeader },
     computed: {
-        cards() {
-            return cards
-        },
-        categories() {
-            return categories
-        },
         info() {
             return info
+        },
+        cardsGrouped() {
+            return categories.map(cat => ({
+                ...cat,
+                cards: cards.filter(card => card.category === cat.id)
+            }))
         }
     }
 }
 </script>
 
 <style scoped>
+.category {
+    margin: 4rem auto;
+    max-width: 1130px;
+}
+
 .grid {
     display: grid;
     grid-template-columns: repeat(auto-fit, 15rem);
-    margin: 4rem auto;
     justify-content: space-around;
-    max-width: 1130px;
     grid-gap: 3rem;
 }
 </style>
